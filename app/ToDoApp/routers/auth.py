b44bad1f43727381d96a1bd5c2935d1fa243cc25
Ali Khajoei <119170917+Alikhajoei@users.noreply.github.com>
2025-03-04 17:17:25 +0330

    added passlib for hash password and commit to db

--- app/ToDoApp/routers/auth.py
+++ app/ToDoApp/routers/auth.py
@@ -2,9 +2,11 @@ from fastapi import HTTPException,APIRouter,Path
 from starlette import status
 from pydantic import BaseModel
 from models import Users
+from passlib.context import CryptContext
 
 router = APIRouter()
 
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 class UserRequest(BaseModel):
     username: str = Path(min_length=3, max_length=20)
@@ -14,5 +16,5 @@ class UserRequest(BaseModel):
 
 @router.post("/register/",status_code=status.HTTP_201_CREATED)
 async def user_register(create_user_request: UserRequest):
-    new_user = Users(username=create_user_request.username, hashed_password=create_user_request.password,is_active=True)
+    new_user = Users(username=create_user_request.username, hashed_password=pwd_context.hash(create_user_request.password),is_active=True)
     return new_user
